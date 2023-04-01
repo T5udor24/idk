0328fccaa7e34cb594c6c042aa46c6718206bda4
T5udor24 <129603665+T5udor24@users.noreply.github.com>
2023-04-01 16:44:55 +0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,15 @@
+from pygame import*
+window = display.set_mode((700, 500))
+display.set_caption("First project")
+#character coordinates
+x = 100
+y = 395
+#character and background image
+img1 = transform.scale(image.load('monkey_001.png'), (100, 100))
+background = transform.scale(image.load("cave.png"), (700,500))
+#we place the images on the application window
+window.blit(background,(0,0))
+window.blit(img1,(x2,y2))
+#refresh the window
+display.update()
+time.delay(5000)
